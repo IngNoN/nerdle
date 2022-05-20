878b2d254e4eb1203a4fc5de141fd9aea920ebf1
Sebastian Karner <sebastian.karner@bulme.at>
2022-05-20 09:07:57 +0200

    basic is working

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from cgitb import small
 from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox
 from nerdle_gui import Ui_MainWindow
 
+from collections import Counter
 class MyWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
@@ -45,61 +47,65 @@ class MyWindow(QMainWindow, Ui_MainWindow):
             self.wirte_num_2_label(op_text)
 
     def enter_button(self):
-        # self.label18.setStyleSheet("background-color : yellow")
+
         validator = True
+        # check for empty labels
         for i in self.labels[self.row]:
             if i.text() == "":
                 validator = False
                 break
-        print(validator)
+
         if validator:
-            self.small_border()
-            self.big_border()
+
             self.check_input()
-                # self.label18.setStyleSheet("background-color : yellow")
         
     def check_input(self):
-        # self.label18.setStyleSheet("background-color : yellow")
-        # self.label17.setStyleSheet("background-color : yellow")
         self.input_str = ""
 
         for i in self.labels[self.row]:
             self.input_str = self.input_str + i.text()
 
         split_input_str = self.input_str.split("=")
+
         if eval(split_input_str[0]) == int(split_input_str[1]):
             self.check_input_right_numbers()
-            # self.label18.setStyleSheet("background-color : yellow")
             if self.input_str == self.result:
-                print("right")
                 self.game_state = "finished"
                 return False # If this is the right string the game should stop
             else:
-                self.row += 1
-                self.col = 0
-                self.big_border()
-                print("false")
-                return True
+                if self.check_for_last_row():
+                    self.game_running()
+                else:
+                    dialog = QMessageBox(self)
+                    dialog.setWindowTitle("Error")
+                    dialog.setText("No guesses left!")
+                    dialog.exec()
         else:
-            print("nicht richtiges ergebnis")
             dialog = QMessageBox(self)
             dialog.setWindowTitle("Error")
             dialog.setText("That guess doesn't compute")
             dialog.exec()
-        
+    
+    def game_running(self):
+        self.row += 1
+        self.col = 0
+        self.small_border()
+        self.big_border()
+
+
     def check_input_right_numbers(self):
-        right_num = []
-        
-        for i in range(len(self.labels[self.row])):
-            if self.input_str[i] in self.result:
-                self.labels[self.row][i].setStyleSheet("background-color : yellow")
-                #print(i, self.labels[self.row][i].text())
-                right_num.append(self.input_str[i])
+        num_amount_dict = Counter(self.result)
+
+        j = 0
+        for i in self.input_str:
+            if num_amount_dict[i] > 0:
+                self.labels[self.row][j].setStyleSheet("background-color: yellow")
+                num_amount_dict[i] -= 1
             else:
-                right_num.append("")
-                self.labels[self.row][i].setStyleSheet("background-color: red")
-        #print(right_num)
-        self.check_input_right_position(right_num)
+                self.labels[self.row][j].setStyleSheet("background-color: red")
+            j += 1
+
+        self.check_input_right_position(self.input_str)
 
     def check_input_right_position(self, right_numbers):
         for i in range(len(self.labels[self.row])):
@@ -107,6 +113,12 @@ class MyWindow(QMainWindow, Ui_MainWindow):
             if right_numbers[i] == self.result[i]:
                 self.labels[self.row][i].setStyleSheet("background-color : green")
 
+    def check_for_last_row(self):
+        if self.row == 5:
+            self.game_state = "game over"
+            return False
+        else:
+            return True
 
     def deltete_button(self):
         if self.game_state == "running":
